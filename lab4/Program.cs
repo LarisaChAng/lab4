1c99dfa62b8eb9e985c405b84d92ca05e72882de
Лариса <@LAPTOP-BSVAG2QO>
2022-10-21 14:42:09 +0800

    1испрДобавьте GITIGNORE- и GITATTRIBUTES-файлы.

---
+++ lab4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //i=0;
+            for (int i = 0; i < 10; i++)
+                // без усл цикл станет бескон-ым
+            {
+                if (i == 7)
+                    // break;
+                    // continue;
+              
+                Console.WriteLine("Квадрат числа {0} равен {1}", i, i * i);
+                // Console.WriteLine("Квадрат числа {0} равен {1}", i++, i * i); - но вывод от 1 до 10 (а не с 0)
+                //i++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
